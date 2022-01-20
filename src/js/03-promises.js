3dce99ef853dde4bb3ba8b3a7b1f7d26ec3ebbe6
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-01-20 19:24:20 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const userDelayEl = document.querySelector('input[name="delay"]');
 const userStepEl = document.querySelector('input[name="step"]');
 const userAmountEl = document.querySelector('input[name="amount"]');
@@ -36,9 +38,9 @@ setTimeout(() => {
   });
   promise
   .then(({ position, delay }) => {
-    console.log(`Fulfilled promise ${position} in ${delay}ms`);
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
-    console.log(`Rejected promise ${position} in ${delay}ms`);
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
   });
 };
